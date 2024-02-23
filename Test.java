f6902e8f8661169e831355ec4e853ac253178835
ahmedomair77 <141590050+ahmedomair77@users.noreply.github.com>
2024-02-23 09:18:44 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 class Test { 
-	public static void main(String args[])
+	public (String args[])
 	{ 
 		System.out.println("Hello  World");
 		System.out.println("This task is performed by Jenkins linked with GitHub Repository and Slack.");
